d1df19dd707b7fb30dac86f48ee40e2310950772
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-12 21:11:41 -0300

    Update

--- app/src/main/java/com/example/loginviewconstraintlayout/UserActivity.kt
+++ app/src/main/java/com/example/loginviewconstraintlayout/UserActivity.kt
@@ -1,20 +1,31 @@
 package com.example.loginviewconstraintlayout
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.loginviewconstraintlayout.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(),  View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_user)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.button.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        Toast.makeText(this, "Congratulations, you got it!", Toast.LENGTH_SHORT).show()
     }
 }
